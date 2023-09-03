f1b8ef59ee1c8e158bd420b3b6b7ce223af884ab
Rajan Jaiswal <rajan@Rajans-MacBook-Pro.local>
2023-09-03 21:46:48 +0530

    Updated AR links

--- assets/config.js
+++ assets/config.js
@@ -5,27 +5,27 @@ let config = {
   "links": [
     {
       "Title": "Natural Gas (Including NG Marketing, NG Pipeline, LNG Insfrastucture, Sourcing)",
-      "URL": "https://mywebar.com/p/gail",
+      "URL": "https://mywebar.com/p/gail1",
     },
     {
       "Title": "CGD (City Gas Distribution)",
-      "URL": "https://mywebar.com/p/gail",
+      "URL": "https://mywebar.com/p/gail2",
     },
     {
       "Title": "Future area of diversification (Renewables -Wind & Solar, Chemicals, Speciality Chemicals, Hydrocarbons, CBG, Hydrocarbons)",
-      "URL": "https://mywebar.com/p/gail",
+      "URL": "https://mywebar.com/p/gail3",
     },
     {
       "Title": "Petrochemicals (Including Polymers & Liquid Hydrocarbons)",
-      "URL": "https://mywebar.com/p/gail",
+      "URL": "https://mywebar.com/p/gail4",
     },
     {
       "Title": "Sustainability",
-      "URL": "https://mywebar.com/p/gail",
+      "URL": "https://mywebar.com/p/gail5",
     },
     {
       "Title": "CSR (Corporate Social Responsibility)",
-      "URL": "https://mywebar.com/p/gail",
+      "URL": "https://mywebar.com/p/gail6",
     },
   ]
 }
