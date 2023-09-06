509cbb3556c523c4c3c28c8ea45f8b35c50233b8
Rajan Jaiswal <rajan@Rajans-MacBook-Pro.local>
2023-09-06 13:22:36 +0530

    Added back buttons and loop video

--- assets/js/video.js
+++ assets/js/video.js
@@ -6,22 +6,22 @@ video_container = document.getElementById("video-container")
 
 switch(url) {
     case "1":
-        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/OxWqSe4MPXA?autoplay=1&mute=1")
+        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/OxWqSe4MPXA?autoplay=1&mute=1&playlist=OxWqSe4MPXA&loop=1")
         break;
     case "2":
-        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/T8COD3adhys?autoplay=1&mute=1")
+        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/T8COD3adhys?autoplay=1&mute=1&playlist=T8COD3adhys&loop=1")
         break;
     case "3":
-        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/yIY4V7DlD2g?autoplay=1&mute=1")
+        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/yIY4V7DlD2g?autoplay=1&mute=1&playlist=yIY4V7DlD2g&loop=1")
         break;
     case "4":
-        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/k5DInlUaZ90?autoplay=1&mute=1")
+        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/k5DInlUaZ90?autoplay=1&mute=1&playlist=k5DInlUaZ90&loop=1")
         break;
     case "5":
-        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/BPtvW9_tzL0?autoplay=1&mute=1")
+        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/BPtvW9_tzL0?autoplay=1&mute=1&playlist=BPtvW9_tzL0&loop=1")
         break;
     case "6":
-        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/qok1Vbegehw?autoplay=1&mute=1")
+        video_container.innerHTML = videoComponent("https://www.youtube.com/embed/qok1Vbegehw?autoplay=1&mute=1&playlist=qok1Vbegehw&loop=1")
         break;
     default:
         video_container.innerHTML = videoComponent(url)
--- assets/js/video_component.js
+++ assets/js/video_component.js
@@ -4,6 +4,12 @@ const videoComponent = (url) => {
         <iframe frameborder="0" width="100%" height="100%"
             src="${url}">
         </iframe>
+        <form class="back">
+            <input class="button" type="button" value="Go back" onclick="history.back()">
+        </form>
+        <form class="home">
+            <input class="button" type="button" value="Go home" onclick="history.go(-2)">
+        </form>
     </div>
     `
 }
